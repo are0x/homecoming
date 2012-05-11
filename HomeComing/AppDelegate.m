007e2d6ef810c606e39ac66004c8500a6249939f
are0x <can_i_ride_a_earth@hotmail.co.jp>
2012-05-12 00:08:16 +0900

    first commit

---
+++ HomeComing/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  HomeComing
+//
+//  Created by arai takahiro on 12/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
