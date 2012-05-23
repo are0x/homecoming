39ec5cca04841fb9ebe16926c54a585139cca277
Shoma Ishihara <ishihara.shoma@gmail.com>
2012-05-24 02:03:56 +0900

    イベントを処理できるようにした

---
+++ HomeComing/StoryWindowController.mm
@@ -0,0 +1,34 @@
+//
+//  StoryWindowController.m
+//  HomeComing
+//
+//  Created by 翔真 石原 on 12/05/23.
+//  Copyright (c) 2012年 東京工業大学. All rights reserved.
+//
+
+#import "StoryWindowController.h"
+
+@interface StoryWindowController ()
+
+@end
+
+@implementation StoryWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
